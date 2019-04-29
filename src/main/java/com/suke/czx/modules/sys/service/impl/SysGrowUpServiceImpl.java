44d192ff8d219959535269f5c0d0f5a50b6dbd4e
shazhengqiu <shazq@mvtech.com.cn>
2019-04-29 21:15:00 +0800

    fix # 上传附件方法

--- src/main/java/com/suke/czx/modules/sys/service/impl/SysGrowUpServiceImpl.java
+++ src/main/java/com/suke/czx/modules/sys/service/impl/SysGrowUpServiceImpl.java
@@ -1,11 +1,15 @@
 package com.suke.czx.modules.sys.service.impl;
 
+import com.suke.czx.common.utils.ConfigConstant;
 import com.suke.czx.modules.sys.dao.SysGrowUpDao;
 import com.suke.czx.modules.sys.entity.SysLogEntity;
 import com.suke.czx.modules.sys.service.SysGrowUpService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -20,16 +24,34 @@ public class SysGrowUpServiceImpl implements SysGrowUpService{
      * 保存成长册信息
      */
     public void save(Map<String,Object> map){
-        String uuid = UUID.randomUUID().toString();
-
-        Map<String,Object> growUp =new HashMap<String,Object>();
-
-        map.put("id",uuid);
         //   保存成长册信息
         sysGrowUpDao.save(map);
-        map.put("fk_id",uuid);
-        //   保存附件
-        sysGrowUpDao.saveFile(map);
+    }
+    /**
+     * 保存成长册信息
+     */
+    public void saveFile(MultipartFile multipartFile, String userName,String uuid){
+        try {
+            if(multipartFile!=null){
+                String filename=multipartFile.getName();
+                String suffix =filename.substring(filename.lastIndexOf("."));
+                File files=new File(ConfigConstant.FILE_BASE_PATH);
+                if(!files.exists()){
+                    files.mkdirs();
+                }
+                File file = new File(ConfigConstant.FILE_BASE_PATH+filename);
+                multipartFile.transferTo(file);
+                Map<String,Object> params =new HashMap<String,Object>();
+                params.put("accessoryName",filename);
+                params.put("accessorySuffix",suffix);
+                params.put("accessoryAddress",ConfigConstant.FILE_BASE_PATH+filename);
+                params.put("id",uuid==null?"":uuid);
+                params.put("createUser",userName);
+                sysGrowUpDao.saveFile(params);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override

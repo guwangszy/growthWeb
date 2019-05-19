b0e0f615217ed82d6a0393a1433a9f8d4d6f8222
shazhengqiu <shazq@mvtech.com.cn>
2019-05-19 23:29:14 +0800

    fix # 添加背景图片

--- src/main/java/com/suke/czx/config/ShiroConfig.java
+++ src/main/java/com/suke/czx/config/ShiroConfig.java
@@ -69,6 +69,7 @@ public class ShiroConfig {
         filterMap.put("/swagger/**", "anon");
         filterMap.put("/favicon.ico", "anon");
         filterMap.put("/captcha.jpg", "anon");
+        filterMap.put("/bg.jpg", "anon");
         filterMap.put("/", "anon");
         filterMap.put("/**", "oauth2");
         shiroFilter.setFilterChainDefinitionMap(filterMap);
